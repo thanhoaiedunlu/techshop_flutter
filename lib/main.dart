4307f94274a35d8712351c679fedaa08dcfa1fb7
thanhoaiedunlu <21130360@st.hcmuaf.edu.vn>
2024-12-15 15:15:41 +0700

    qr

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:techshop_flutter/screens/login/login.dart';
-
+import 'package:techshop_flutter/screens/login/signUp.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const Login(),
     );
   }
 }
